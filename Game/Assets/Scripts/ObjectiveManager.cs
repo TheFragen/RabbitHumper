3e08922eec355df22352a1516a13ac0d2c4eb141
JoakimHaurum <joakim@bruslund-haurum.dk>
2016-03-04 22:25:21 +0100

    Added objective script and objective spawner script

---
+++ Game/Assets/Scripts/ObjectiveManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveManager : MonoBehaviour {
+
+	private int index;
+
+
+	/***  TEST VARIABLES ***/
+	public float testTime = 2.0f;
+	private float testLifeTimer;
+
+
+	// Use this for initialization
+	void Start () {
+		testLifeTimer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Time.time >= testLifeTimer + testTime) {
+			//ObjectiveSpawnerManager.instance.setSpaceOccupied(index);
+			//Destroy (gameObject);
+		}
+
+	}
+
+	public void setIndex(int i){
+		index = i;
+	}
+
+}
---
+++ Game/Assets/Scripts/ObjectiveSpawnerManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectiveSpawnerManager : MonoBehaviour {
+	public static ObjectiveSpawnerManager instance = null;
+
+	public GameObject objective;
+	public List<GameObject> spawnPoints = new List<GameObject>();
+
+	public float minTime = 2.0f;
+	public float maxTime = 4.0f;
+
+	private float startTime;
+	private float spawnTimer;
+	private int lastIndex;
+	private bool[] spaceOccupied;
+
+	void Awake(){
+		//Makes the manager a singleton which persists through scenes
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		spawnTimer = Random.Range (minTime, maxTime);
+		lastIndex = spawnPoints.Count + 1;
+		spaceOccupied = new bool[spawnPoints.Count];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time >= startTime + spawnTimer) {
+			int index = Random.Range (0, spawnPoints.Count);
+
+			if (index != lastIndex && spaceOccupied[index] == false) {
+				GameObject newObj = Instantiate (objective, spawnPoints [index].transform.position, Quaternion.identity) as GameObject;
+				newObj.GetComponent<ObjectiveManager>().setIndex (index);
+				spaceOccupied [index] = true;
+				lastIndex = index;
+				startTime = Time.time;
+			}
+		}
+	
+	}
+
+	public void setSpaceOccupied(int i){
+		spaceOccupied [i] = false;
+	}
+}
