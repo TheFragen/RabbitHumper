fe959fd768275dd5d1252be1cd52cc20f4a6778e
Chrisitan Fæster <christian.faester@yahoo.dk>
2016-03-04 23:58:10 +0100

    Actual real movement!

---
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+    public float acceleration = 10;
+    public float jumpSpeed = 10;
+    public float maxVelocity;
+    float distToGround;
+    public bool isJumping = false;
+    Rigidbody rb;
+    Vector3 moveDir = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+
+        distToGround = this.GetComponent<Collider>().bounds.extents.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        isJumping = !isGrounded();
+
+
+    }
+    
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            moveDir = new Vector3(0, 0, Input.GetAxis("Horizontal"));
+            if (Mathf.Abs(rb.velocity.z) <= maxVelocity)
+            {
+                rb.AddForce(moveDir * acceleration);
+            }
+        } else if (!isJumping)
+        {
+            rb.velocity = rb.velocity / 1.2f;
+        }
+
+        if (Input.GetButton("Fire2") && !isJumping)
+        {
+            rb.AddForce(new Vector3(0, 100, 0) * jumpSpeed);
+        }
+    }
+
+    public bool isGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
