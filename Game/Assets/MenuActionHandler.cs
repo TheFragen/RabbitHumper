b5676f8e071d86d3ef0b0abe948b3fb8fb58f263
JoakimHaurum <joakim@bruslund-haurum.dk>
2016-03-06 02:47:49 +0100

    Adapting menu scene to work with joystick

--- Game/Assets/MenuActionHandler.cs
+++ Game/Assets/MenuActionHandler.cs
@@ -1,16 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using XboxCtrlrInput;
+using UnityEngine.UI;
 
 public class MenuActionHandler : MonoBehaviour {
 
+	XboxController controller1;
+	XboxController controller2;
+
+	public Text Text1, Text1P, Text2, Text2P;
+
+	bool player1 = false;
+	bool player2 = false;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		controller1 = XboxController.First;
+		controller2 = XboxController.Second;
+		Text1.transform.gameObject.SetActive (true);
+		Text1P.transform.gameObject.SetActive (false);
+		Text2.transform.gameObject.SetActive (true);
+		Text2P.transform.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (!player1 && XCI.GetButton (XboxButton.A, controller1)) {
+			player1 = true;
+			Text1.transform.parent.gameObject.SetActive (false);
+			Text1P.transform.parent.gameObject.SetActive (true);
+		}
+		if (!player2 && XCI.GetButton (XboxButton.A, controller2)) {
+			player2 = true;
+			Text2.transform.parent.gameObject.SetActive (false);
+			Text2P.transform.parent.gameObject.SetActive (true);
+		}
+			
+		if (player1 && player2 || Input.GetKey(KeyCode.Return)) {
+			loadLevel (1);
+		}
 	
 	}
 
